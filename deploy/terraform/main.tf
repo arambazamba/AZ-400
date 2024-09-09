ff9cb4959fe80ee33a9925f2a60459886b4e6595
Alexander Kastil <alexander.kastil@integrations.at>
2024-09-09 08:16:12 +0200

    tf

--- deploy/terraform/main.tf
+++ deploy/terraform/main.tf
@@ -5,11 +5,20 @@ terraform {
             version = ">= 4.00"
         }
     }
+    random = {
+            source = "hashicorp/random"
+            version = ">= 3.0.0"
+        }
 }
 
 provider "azurerm" {
     features {}
     subscription_id = "78033352-805c-4acd-af80-f8f95083268d"
+}
+
+resource "azurerm_resource_group" "az400-dev" {
+    name = "az400-dev"
+    location = "westeurope"
 }
 
 resource "azurerm_service_plan" "az400-dev" {
