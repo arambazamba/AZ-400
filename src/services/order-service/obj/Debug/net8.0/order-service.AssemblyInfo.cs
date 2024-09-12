e88b3c86e889f64a787a2052fc2bdba5cd3752cf
Alexander Kastil <alexander.kastil@integrations.at>
2024-09-12 19:41:59 +0200

    work-item-gate

--- src/services/order-service/obj/Debug/net8.0/order-service.AssemblyInfo.cs
+++ src/services/order-service/obj/Debug/net8.0/order-service.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("order-service")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d26cb9294eb33ec761986a506200e9595daa6daa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+14f131ec2d4de2a2745d9a58d5a9d53a96d1cc6a")]
 [assembly: System.Reflection.AssemblyProductAttribute("order-service")]
 [assembly: System.Reflection.AssemblyTitleAttribute("order-service")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
