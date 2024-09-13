0e62e321899b3979982ec1a01e1c201814baf4d9
Alexander Kastil <alexander.kastil@integrations.at>
2024-09-13 07:35:45 +0200

    mock data

--- src/web/food-shop/src/app/food/state/cart/mock-data.ts
+++ src/web/food-shop/src/app/food/state/cart/mock-data.ts
@@ -1,7 +1,7 @@
 
 export const mockOrder = {
-  customer: { id: '1', name: 'Alexander Kastil', phone: '+43 664 436 07 77', email: 'alexander.kastil@integrations.at' },
+  customer: { id: '1', name: 'Giro Kastil', phone: '+43 664 436 07 77', email: 'giro.kastil@integrations.at' },
   payment: { type: 'Bank Account', accountNumber: '1234' },
-  shippingAddress: { street: 'Am Himmel 1', city: 'Wien', country: 'Austria', zipCode: '1190' },
+  shippingAddress: { street: 'Himmelstraße 115', city: 'Wien', country: 'Austria', zipCode: '1190' },
   items: []
 }
