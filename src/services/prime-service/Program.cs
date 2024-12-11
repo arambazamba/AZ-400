10ad6957e9e6d80d7ca568f9aefcb75b95de72f9
Alexander Kastil <alexander.kastil@integrations.at>
2024-12-11 12:04:29 +0100

    Refactor Azure CLI scripts and update JMeter test plan (#152)
    * Refactor Azure CLI scripts for container deployment and update JMeter test plan to use latest version
    
    * Add environment variable for catalog API URL in ACI deployment script

--- src/services/prime-service/Program.cs
+++ src/services/prime-service/Program.cs
@@ -1,9 +1,9 @@
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -12,7 +12,11 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 
 app.UseSwagger();
-app.UseSwaggerUI();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+    c.RoutePrefix = string.Empty; // Set Swagger UI at the app's root
+});
 
 app.UseHttpsRedirection();
 
